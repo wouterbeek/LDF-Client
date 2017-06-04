24e7942c2bb8319ead7fd4d434b48b58f48c2d65
Wouter Beek <wouter@triply.cc>
2017-06-04 11:00:41 +0200

    V0.0.5 Fixed Pack definition.

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 author('Wouter Beek', 'wouter@triply.cc').
-download('https://github.com/wouterbeek/ldf_client/releases/*.zip').
+download('https://github.com/wouterbeek/ldf_client/releases/tag/*.zip').
 keywords(['API',client,'LDF','Linked Data Fragments']).
 name(ldf_client).
 requires(rdf_client).
 title('A Linked Data Fragment (LDF) Client for SWI-Prolog.').
-version('0.0.4').
+version('0.0.5').
